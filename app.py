0dbb9d97bcc065b99a1b8dc9a371d16b0862a75f
Saurabh Verma <s@3w3.one>
2025-02-12 14:48:03 +0530

    revision 2

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_next_user_id():
 # -------------------------------------
 @app.route('/usermanagement')
 def home():
-    return "Welcome to the Banking Platform - User Management API! Revision 1"
+    return "Welcome to the Banking Platform - User Management API! Revision 2"
 
 # -------------------------------------
 # Endpoint: List All Users
