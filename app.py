5993ca69581d2e424fa4e251469160c1b9ce25f4
Saurabh Verma <s@3w3.one>
2025-02-12 17:17:10 +0530

    adding prod manifests

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_next_user_id():
 # -------------------------------------
 @app.route('/usermanagement')
 def home():
-    return "Welcome to the Banking Platform - User Management API! Revision 6"
+    return "Welcome to the Banking Platform - User Management API! Revision 7"
 
 # -------------------------------------
 # Endpoint: List All Users
